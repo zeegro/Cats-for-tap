28cfff722c65303ba30b6ef5149ea5bac4d67ec6
Zeegro <mr.grobov@list.ru>
2023-09-16 05:55:36 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Получаем ссылки на кнопки и секции
+const catsButton = document.getElementById("cats-button");
+const collectionButton = document.getElementById("collection-button");
+const sectionCats = document.getElementById("section-cats");
+const sectionCollection = document.getElementById("section-collection");
 
+// Обработчики событий для кнопок
+catsButton.addEventListener("click", () => {
+    sectionCats.scrollIntoView({ behavior: "smooth" });
+});
+
+collectionButton.addEventListener("click", () => {
+    sectionCollection.scrollIntoView({ behavior: "smooth" });
+});
